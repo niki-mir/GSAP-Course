cecaa272ec05145cae9ff8ea2363cb0ad4526860
niki-mir <nikimir00@gmail.com>
2024-09-04 16:35:07 +0200

    2.2

---
+++ test/2.2.js
@@ -0,0 +1,26 @@
+// titleProject название проекта (Оценка стоимости)
+// screensValue строка с названиями типов экранов через запятую ("шаблонные, с уникальным дизайном, с анимациями"),
+// screenPrice любое число (цена реализации одного экрана)
+// percentage любое число от 1 до 100 (процент отката подрядчику),
+// fullPrice любое число (имеется ввиду, желаемая сумма зароботка с проекта)
+// responsive булевое значение
+
+
+const titleProject = 'Оценка Стоимости'
+console.log(typeof titleProject)
+
+const screensValue = String('шаблонные, с уникальным дизайном, с анимациями')
+console.log(typeof screensValue)
+console.log(screensValue)
+
+const screenPrice = 10500
+console.log(typeof screenPrice)
+
+const percentage = 10
+console.log(typeof percentage)
+
+const fullPrice = 180000
+console.log(typeof fullPrice)
+
+const responsive = true
+console.log(typeof responsive)
